0a0f6cf4b6c76f68360dce342edb12b6a49cc404
constuelo <ferguson.rory@gmail.com>
2019-05-20 09:22:35 +0100

    Added styles

--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 
-let gameBoxStyles = {
-    'background-color': 'whitesmoke',
-    'padding': '20px',
-    'text-align': 'center',
-    'margin': '20px',
-    'border-radius': '5px',
-}
+
+const GameBoxContainer = styled.div`
+    background-color: whitesmoke;
+    padding: 20px;
+    text-align: center;
+    margin: 20px;
+    border-radius: 5px;
+`
+const GameBoxImage = styled.img`
+    width: 200px;
+    height: auto;
+`
 
 class GameBox extends Component {
     render() {
         return (
-            <div style={gameBoxStyles}>
-                
-                <img src={this.props.image} width="200" height="auto" alt="" />
+            <GameBoxContainer>
+                <GameBoxImage src={this.props.image} alt="" />
                 <h3>{this.props.title}</h3>
                 <p>Release Date: {this.props.release}</p>
-            </div>
+            </GameBoxContainer>
         )
     };
 }
